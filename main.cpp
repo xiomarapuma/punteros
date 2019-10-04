7b6892e4e73e73c2c770b568b5c4582f16c40fa1
xiomarapuma <54367624+xiomarapuma@users.noreply.github.com>
2019-10-04 09:42:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 using namespace std;
-
+//print
 int imprimir(int *arr , int tam){
     for (int i=0;i<tam ;i++){
         cout << *arr << " "  ;
         *arr ++ ;
     }
 }
-///suma de elementos de un arreglo
+///suma de elementos de un arreglo con punteros
 int suma(int arr[],int tam){ ///recibe una direccion de memoria int arr[]== int *arr
      int suma=0 ;
      for(int i=0;i<tam ; i++){
@@ -16,7 +16,7 @@ int suma(int arr[],int tam){ ///recibe una direccion de memoria int arr[]== int 
         arr++;
      }
 }
-//suma de elementos de un arreglo recursiva
+//suma de elementos de un arreglo recursiva punteros
 int sumaRecursiva(int *arr,int *tam){
     if (arr==tam){
         return *arr; //a[0]
@@ -25,29 +25,45 @@ int sumaRecursiva(int *arr,int *tam){
         return *arr+sumaRecursiva(arr+1,tam);
     }
 }
-
-/*
-void invertir(int *arr ,int tam){
-    arr [] = *ptr ;
-    tam-- ;
-    +
-
-    for(int i=tam ; i>=0;i--){
-        ptr= &arr[i];
-        cout << *ptr<< " " ;
-
+// invertir 
+void invertir(int *arr, int tam){ //iterativa y recursiva
+    tam=tam-1;
+    int k =tam/2;
+    for (int i=0;i<k;i++){
+        int b = *(arr+i);
+        *(arr+i)=*(arr+tam);
+        *(arr+tam)=b;
+        tam=tam-1;
     }
 }
-*/
+//invertir recursiva
+void invertirRecursiva(int *arr,int tam, int i=0){
+    int k=tam-1;
+    if(i<tam){
+        int a = *(arr+i);
+        *(arr+i) =*(arr+k);
+        *(arr+k)= a;
+
+        invertirRecursiva(arr,--tam,++i);
+    }
 
 int main()
 {
     int x[]={5,2,3,4};
     imprimir(x,4);
     
-    int x[]={4,5,7,8,9,4};
-
-    cout << sumaRecursiva(x,x+5);
+    int x[]={4,5,7,8,9,1};
+    cout << sumaRecursiva(x,x+5)<< endl ;
+    imprimir(x,6);
+    cout << endl ;
+    invertir(x,6);
+    cout <<endl ;
+    imprimir(x,6);
+    cout <<endl ;
+    invertirRecursiva(x,6);
+    cout <<endl ;
+    imprimir(x,6);
+    
     /*
     cout << *arr << endl ;
     int *ptr;
